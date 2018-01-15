fd28d429e2640cc74cf780f2a5c25770d92fddf7
weiqiang chen <weiqiangchen@weiqiangdemac.local>
2018-01-15 16:44:06 +0800

    Initial Commit

---
+++ WQDevDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WQDevDemo
+//
+//  Created by weiqiang chen on 2018/1/15.
+//  Copyright © 2018年 weiqiang chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WQDevDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WQDevDemo
+//
+//  Created by weiqiang chen on 2018/1/15.
+//  Copyright © 2018年 weiqiang chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
